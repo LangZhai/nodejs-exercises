ba5a50e9c7122e94ac8f32feea70115eec243af9
LangZhai <zl2012xyz@hotmail.com>
2016-04-26 14:50:11 +0800

    Daily update

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express'),
     path = require('path'),
-    favicon = require('serve-favicon'),
     logger = require('morgan'),
     cookieParser = require('cookie-parser'),
     bodyParser = require('body-parser'),
@@ -11,7 +10,6 @@ var express = require('express'),
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
