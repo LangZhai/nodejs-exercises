f82cd7ed018b7b79ca660e797f2b6f8453d0bce2
LangZhai <zl2012xyz@hotmail.com>
2016-12-26 11:12:30 +0800

    Daily update

--- resorce/js/frames/index.js
+++ resorce/js/frames/index.js
@@ -7,26 +7,29 @@ $(function () {
         $ul = $('>ul', $nav),
         $footer = $('body>footer'),
         $result = $('>p>output', $footer),
+        $target = $(),
         w = $article.width() * .5,
-        h = $article.height() * .5;
+        h = $article.height() * .5,
+        deal = function (offset, move) {
+            var $parent = $target.parent(),
+                $children = $parent.children(),
+                position = $target.position(),
+                txt;
+            $target.css({left: position.left + move.offsetX - offset.offsetX, top: position.top + move.offsetY - offset.offsetY});
+            txt = [8, $children.length * .2].concat($.map($children, function (item) {
+                var $this = $(item),
+                    position = $this.position();
+                return [w - position.left, h - position.top, $this.width(), $this.height()];
+            })).join(',');
+            $result.eq($parent.index()).text(txt).end().next().attr('href', '/frames/save/?txt=' + txt);
+        };
 
     $section.on({
         dragstart: function (e) {
-            $(this).data('offset', {offsetX: e.offsetX, offsetY: e.offsetY});
+            $target.data('offset', {offsetX: e.offsetX, offsetY: e.offsetY});
         },
         dragend: function (e) {
-            var $this = $(this),
-                $parent = $this.parent(),
-                $children = $parent.children(),
-                position = $this.position(),
-                offset = $this.data('offset'),
-                txt = [8, $children.length * .2].concat($.map($children, function (item) {
-                    var $this = $(item),
-                        position = $this.position();
-                    return [w - position.left, h - position.top, $this.width(), $this.height()];
-                })).join(',');
-            $this.css({left: position.left + e.offsetX - offset.offsetX, top: position.top + e.offsetY - offset.offsetY});
-            $result.eq($parent.index()).text(txt).end().next().attr('href', '/frames/save/?txt=' + txt);
+            deal($target.data('offset'), {offsetX: e.offsetX, offsetY: e.offsetY});
         }
     }, '>figure.curr');
     $nav.on('click', function () {
@@ -35,9 +38,9 @@ $(function () {
     });
     $ul.on('click', '>li', function () {
         var $this = $(this),
-            $parent = $this.parent().parent(),
-            $target = $section.eq($parent.index());
-        $this.add($target.children(':eq(' + $this.index() + ')')).add($target).addClass('curr').siblings().removeClass('curr');
+            $parent = $section.eq($this.parent().parent().index());
+        $target = $parent.children(':eq(' + $this.index() + ')');
+        $this.add($target).add($parent).addClass('curr').siblings().removeClass('curr');
     });
     $footer.on('change', 'input[type=file]', function (event) {
         var index = $(this).parent().index(),
@@ -51,5 +54,28 @@ $(function () {
         }
         $section.eq(index).html($template_figure.template(data));
         $ul.eq(index).html($template_li.template(data));
+    });
+
+    $(document).on('keydown', function (e) {
+        if ($target.length) {
+            switch (e.keyCode) {
+            case 37:
+                e.preventDefault();
+                deal({offsetX: 0, offsetY: 0}, {offsetX: -1, offsetY: 0});
+                break;
+            case 38:
+                e.preventDefault();
+                deal({offsetX: 0, offsetY: 0}, {offsetX: 0, offsetY: -1});
+                break;
+            case 39:
+                e.preventDefault();
+                deal({offsetX: 0, offsetY: 0}, {offsetX: 1, offsetY: 0});
+                break;
+            case 40:
+                e.preventDefault();
+                deal({offsetX: 0, offsetY: 0}, {offsetX: 0, offsetY: 1});
+                break;
+            }
+        }
     });
 });
