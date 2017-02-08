cdeaad16039a24a2162af72f89f8dc4c72ba782c
LangZhai <zl2012xyz@hotmail.com>
2017-02-08 19:50:49 +0800

    Daily update

--- routes/frames.js
+++ routes/frames.js
@@ -105,92 +105,91 @@ router.post('/auto', function (req, res) {
                     });
                 };
             fs.readdir(dirIn, function (err, files) {
-                files.forEach(function (item, index) {
-                    var dirPath = path.join(dirIn, item);
-                    count++;
-                    fs.readdir(dirPath, function (err, files) {
-                        if (err) {
-                            res.write(err.message + '\r\n');
-                            if (--count === 0) {
-                                if (--total === 0) {
-                                    res.end();
+                if (err) {
+                    res.write(err.message);
+                    if (--total === 0) {
+                        res.end();
+                    }
+                } else {
+                    var direction = files.length === 5 ? 8 : files.length;
+                    files.forEach(function (item, index) {
+                        var dirPath = path.join(dirIn, item);
+                        count++;
+                        fs.readdir(dirPath, function (err, files) {
+                            if (err) {
+                                res.write(err.message + '\r\n');
+                                if (--count === 0) {
+                                    if (--total === 0) {
+                                        res.end();
+                                    }
                                 }
-                            }
-                        } else {
-                            var num = files.length,
-                                datas = [],
-                                result = [],
-                                width = 0,
-                                height = 0,
-                                pic;
-                            files.forEach(function (fileName, i) {
-                                fs.createReadStream(path.join(dirPath, fileName)).on('error', function (err) {
-                                    res.write(err.message + '\r\n');
-                                    if (--num === 0) {
-                                        if (--count === 0) {
-                                            if (--total === 0) {
-                                                res.write('end' + '\r\n');
+                            } else {
+                                var num = files.length,
+                                    datas = [],
+                                    result = [],
+                                    width = 0,
+                                    height = 0,
+                                    pic;
+                                files.forEach(function (fileName, i) {
+                                    fs.createReadStream(path.join(dirPath, fileName)).pipe(new PNG()).on('parsed', function (data) {
+                                        var arr = Array.from(data),
+                                            alpha = arr.filter(function (item, i) {
+                                                return i % 4 === 3;
+                                            }).map(function (item) {
+                                                return item === 0 ? 0 : 1;
+                                            }),
+                                            w = this.width,
+                                            x1 = w,
+                                            x2 = 0;
+                                        alpha.forEach(function (item, i) {
+                                            var val = i % w;
+                                            if (item === 1) {
+                                                x1 = Math.min(val, x1);
+                                                x2 = Math.max(val, x2);
+                                            }
+                                        });
+                                        if (x1 === w) {
+                                            x1 = 0;
+                                        }
+                                        datas[i] = {data: arr, x1: x1, y1: Math.ceil(alpha.indexOf(1) / w), x2: x2, y2: Math.ceil(alpha.lastIndexOf(1) / w), width: x2 - x1 + 1};
+                                        datas[i].height = datas[i].y2 - datas[i].y1;
+                                        width += datas[i].width;
+                                        height = Math.max(height, datas[i].height);
+                                        txt[index][i] = [400 - datas[i].x1, 535 - datas[i].y1, datas[i].width, datas[i].height];
+                                        if (--num === 0) {
+                                            pic = new PNG({width: width, height: height});
+                                            for (var y = 0; y < height; y++) {
+                                                datas.forEach(function (sub) {
+                                                    if (y < height) {
+                                                        result.push.apply(result, sub.data.slice(((y + sub.y1) * w + sub.x1) * 4, ((y + sub.y1) * w + sub.x2 + 1) * 4));
+                                                    }
+                                                });
+                                            }
+                                            pic.data = Buffer.from(result);
+                                            mkdirs(path.join(outPath, dirOut), null, function () {
+                                                pic.pack().pipe(fs.createWriteStream(path.join(outPath, dirOut, picName[item] + '.png')));
+                                            });
+                                            txt[index] = txt[index].reduce(function (a, b) {
+                                                return a.concat(b);
+                                            });
+                                            res.write(dirPath + '\r\n');
+                                            if (--count === 0) {
+                                                mkdirs(path.join(outPath, dirOut), null, function () {
+                                                    fs.writeFile(path.join(outPath, dirOut, txtName + '.txt'), txt.reduce(function (a, b) {
+                                                        return a.concat(b);
+                                                    }, [direction, files.length]).join());
+                                                    if (--total === 0) {
+                                                        res.end();
+                                                    }
+                                                });
                                             }
                                         }
-                                    }
-                                }).pipe(new PNG()).on('parsed', function (data) {
-                                    var arr = Array.from(data),
-                                        alpha = arr.filter(function (item, i) {
-                                            return i % 4 === 3;
-                                        }).map(function (item) {
-                                            return item === 0 ? 0 : 1;
-                                        }),
-                                        w = this.width,
-                                        x1 = w,
-                                        x2 = 0;
-                                    alpha.forEach(function (item, i) {
-                                        var val = i % w;
-                                        if (item === 1) {
-                                            x1 = Math.min(val, x1);
-                                            x2 = Math.max(val, x2);
-                                        }
                                     });
-                                    if (x1 === w) {
-                                        x1 = 0;
-                                    }
-                                    datas[i] = {data: arr, x1: x1, y1: Math.ceil(alpha.indexOf(1) / w), x2: x2, y2: Math.ceil(alpha.lastIndexOf(1) / w), width: x2 - x1 + 1};
-                                    datas[i].height = datas[i].y2 - datas[i].y1;
-                                    width += datas[i].width;
-                                    height = Math.max(height, datas[i].height);
-                                    txt[index][i] = [400 - datas[i].x1, 535 - datas[i].y1, datas[i].width, datas[i].height];
-                                    if (--num === 0) {
-                                        pic = new PNG({width: width, height: height});
-                                        for (var y = 0; y < height; y++) {
-                                            datas.forEach(function (sub) {
-                                                if (y < height) {
-                                                    result.push.apply(result, sub.data.slice(((y + sub.y1) * w + sub.x1) * 4, ((y + sub.y1) * w + sub.x2 + 1) * 4));
-                                                }
-                                            });
-                                        }
-                                        pic.data = Buffer.from(result);
-                                        mkdirs(path.join(outPath, dirOut), null, function () {
-                                            pic.pack().pipe(fs.createWriteStream(path.join(outPath, dirOut, picName[item] + '.png')));
-                                        });
-                                        txt[index] = txt[index].reduce(function (a, b) {
-                                            return a.concat(b);
-                                        });
-                                        res.write(dirPath + '\r\n');
-                                        if (--count === 0) {
-                                            mkdirs(path.join(outPath, dirOut), null, function () {
-                                                fs.writeFile(path.join(outPath, dirOut, txtName + '.txt'), txt.reduce(function (a, b) {
-                                                    return a.concat(b);
-                                                }, [8, files.length]).join());
-                                                if (--total === 0) {
-                                                    res.end();
-                                                }
-                                            });
-                                        }
-                                    }
                                 });
-                            });
-                        }
+                            }
+                        });
                     });
-                });
+                }
             });
         };
     fs.readFile(path.join(rootPath, 'list.json'), function (err, data) {
