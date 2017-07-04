4e9002b9f95d46d5190540300344cc5456155529
智能小菜菜 <zl2012xyz@hotmail.com>
2017-07-04 14:32:48 +0800

    Daily update

--- routes/websocket.js
+++ routes/websocket.js
@@ -1,8 +1,79 @@
-var router = require('express').Router();
+var router = require('express').Router(),
+    parseObj = (obj, view) => {
+        var offset = 1;
+        Object.keys(obj).sort().forEach(item => {
+            var type = obj[item];
+            if (obj[item] instanceof Object) {
 
-router.ws('/', function (ws) {
-    ws.on('message', function (msg) {
-        ws.send(msg);
+            } else if (type === 'number' && (type = view.getUint8(offset++)) < 8) {
+                switch (type) {
+                    case 0:
+                        obj[item] = view.getUint8(offset);
+                        offset += 1;
+                        break;
+                    case 1:
+                        obj[item] = view.getInt8(offset);
+                        offset += 1;
+                        break;
+                    case 2:
+                        obj[item] = view.getUint16(offset);
+                        offset += 2;
+                        break;
+                    case 3:
+                        obj[item] = view.getInt16(offset);
+                        offset += 2;
+                        break;
+                    case 4:
+                        obj[item] = view.getUint32(offset);
+                        offset += 4;
+                        break;
+                    case 5:
+                        obj[item] = view.getInt32(offset);
+                        offset += 4;
+                        break;
+                    case 6:
+                        obj[item] = view.getFloat32(offset);
+                        offset += 4;
+                        break;
+                    case 7:
+                        obj[item] = view.getFloat64(offset);
+                        offset += 8;
+                        break;
+                }
+            } else {
+                obj[item] = new Uint8Array(view.buffer.slice(++offset, offset + view.getUint8(offset - 1)));
+                offset += obj[item].length;
+                obj[item] = decodeURIComponent(String.fromCharCode.apply(null, obj[item]));
+            }
+        });
+        return obj;
+    };
+
+router.ws('/', (ws) => {
+    ws.on('message', (data) => {
+        var view,
+            opcode;
+        if (data instanceof Buffer) {
+            view = new DataView(new Uint8Array(data).buffer);
+            opcode = view.getUint8(0);
+            switch (opcode) {
+                case 1:
+                    ws.send(JSON.stringify(parseObj({
+                        id: 'string',
+                        age: 'number',
+                        sex: 'number',
+                        name: 'string'
+                    }, view)));
+                    break;
+                case 2:
+                    console.log(data);
+                    break;
+                default:
+                    ws.close(1008, `Unknown opcode ${opcode}`);
+            }
+        } else {
+            ws.close(1008, 'Not Buffer');
+        }
     });
 });
 
