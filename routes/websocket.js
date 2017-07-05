6f14f15222f8ab42b637dd5d154a0521742da76b
智能小菜菜 <zl2012xyz@hotmail.com>
2017-07-05 16:09:01 +0800

    Daily update

--- routes/websocket.js
+++ routes/websocket.js
@@ -1,83 +1,5 @@
 var router = require('express').Router(),
-    clone = (...arguments) => {
-        var args,
-            deep;
-        if (typeof arguments[0] === 'boolean') {
-            args = [].slice.call(arguments, 1);
-            deep = arguments[0];
-        } else {
-            args = [].slice.call(arguments);
-        }
-        args.forEach((obj) => {
-            if (obj instanceof Object) {
-                for (var key in obj) {
-                    if (obj.hasOwnProperty(key)) {
-                        args[0][key] = deep ? clone(deep, {}, obj[key]) : obj[key];
-                    }
-                }
-            } else {
-                args[0] = obj;
-            }
-        });
-        return args[0];
-    },
-    parseObj = (offset, obj, view) => {
-        if (obj instanceof Array) {
-            obj.length = view.getUint8(offset++);
-            obj.join().split(',').forEach((item, i) => obj[i] = clone(true, {}, obj[0]));
-        }
-        if (obj instanceof Array && obj[0] instanceof Object) {
-            obj.forEach(item => offset = parseObj(offset, item, view));
-        } else {
-            Object.keys(obj).sort().forEach(item => {
-                if (obj[item] instanceof Object) {
-                    offset = parseObj(offset, obj[item], view);
-                } else if (obj[item] === 'number') {
-                    switch (view.getUint8(offset++)) {
-                        case 0:
-                            obj[item] = view.getUint8(offset);
-                            offset += 1;
-                            break;
-                        case 1:
-                            obj[item] = view.getInt8(offset);
-                            offset += 1;
-                            break;
-                        case 2:
-                            obj[item] = view.getUint16(offset);
-                            offset += 2;
-                            break;
-                        case 3:
-                            obj[item] = view.getInt16(offset);
-                            offset += 2;
-                            break;
-                        case 4:
-                            obj[item] = view.getUint32(offset);
-                            offset += 4;
-                            break;
-                        case 5:
-                            obj[item] = view.getInt32(offset);
-                            offset += 4;
-                            break;
-                        case 6:
-                            obj[item] = view.getFloat32(offset);
-                            offset += 4;
-                            break;
-                        case 7:
-                            obj[item] = view.getFloat64(offset);
-                            offset += 8;
-                            break;
-                    }
-                } else {
-                    obj[item] = String.fromCharCode.apply(null, new Array(view.getUint8(offset++)).join().split(',').map(() => {
-                        var code = view.getUint16(offset);
-                        offset += 2;
-                        return code;
-                    }));
-                }
-            });
-        }
-        return offset;
-    };
+    bufferify = require('../lib/json-bufferify');
 
 router.ws('/', (ws) => {
     ws.on('message', (data) => {
@@ -95,7 +17,7 @@ router.ws('/', (ws) => {
                         sex: 'number',
                         name: 'string'
                     };
-                    parseObj(1, data, view);
+                    bufferify.decode(1, data, view);
                     ws.send(JSON.stringify(data));
                     break;
                 case 2:
@@ -103,7 +25,7 @@ router.ws('/', (ws) => {
                         index: 'number',
                         name: 'string'
                     }];
-                    parseObj(1, data, view);
+                    bufferify.decode(1, data, view);
                     ws.send(JSON.stringify(data));
                     break;
                 case 3:
@@ -114,12 +36,12 @@ router.ws('/', (ws) => {
                             name: 'string'
                         }]
                     };
-                    parseObj(1, data, view);
+                    bufferify.decode(1, data, view);
                     ws.send(JSON.stringify(data));
                     break;
                 case 4:
                     data = ['number'];
-                    parseObj(1, data, view);
+                    bufferify.decode(1, data, view);
                     ws.send(JSON.stringify(data));
                     break;
                 default:
