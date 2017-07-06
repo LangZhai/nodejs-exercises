5d4940335d3c8e53cb20e8d99f7cf55c3abf9f7b
智能小菜菜 <zl2012xyz@hotmail.com>
2017-07-06 10:51:41 +0800

    Daily update

--- modules/json-bufferify/json-bufferify.d.ts
+++
@@ -1,5 +0,0 @@
-declare namespace bufferify {
-    export function encode(offset: number, data: Object): DataView;
-    export function decode(offset: number, obj: any, source: ArrayBuffer): number;
-    export function decode(offset: number, obj: any, source: DataView): number;
-}
