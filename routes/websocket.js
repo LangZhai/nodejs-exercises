e2ec9aa099aa7d3a8ab0ae6e1f77b36185e5772b
智能小菜菜 <zl2012xyz@hotmail.com>
2017-07-12 15:13:25 +0800

    Daily update

--- routes/websocket.js
+++ routes/websocket.js
@@ -4,6 +4,9 @@ var router = require('express').Router(),
         return Math.floor(Math.random() * ((max ? max : 0) - min + 1)) + min;
     };
 
+/**
+ * 发送指令号枚举
+ */
 var SystemSendOP;
 (function (SystemSendOP) {
     SystemSendOP[SystemSendOP["ENUM_START"] = 0] = "ENUM_START";
@@ -14,6 +17,9 @@ var TestSendOP;
     TestSendOP[TestSendOP["SEND_JSON"] = 1] = "SEND_JSON";
     TestSendOP[TestSendOP["ENUM_END"] = 2] = "ENUM_END";
 })(TestSendOP || (TestSendOP = {}));
+/**
+ * 接收指令号枚举
+ */
 var SystemReturnOP;
 (function (SystemReturnOP) {
     SystemReturnOP[SystemReturnOP["ENUM_START"] = 0] = "ENUM_START";
@@ -26,14 +32,23 @@ var TestReturnOP;
     TestReturnOP[TestReturnOP["RETURN_INFO"] = 3] = "RETURN_INFO";
     TestReturnOP[TestReturnOP["ENUM_END"] = 4] = "ENUM_END";
 })(TestReturnOP || (TestReturnOP = {}));
+/**
+ * 自助指令号枚举
+ */
+var SelfOP;
+(function (SelfOP) {
+    SelfOP[SelfOP["ENUM_START"] = 1000] = "ENUM_START";
+    SelfOP[SelfOP["SELF_HEELO"] = 1001] = "SELF_HEELO";
+    SelfOP[SelfOP["ENUM_END"] = 1002] = "ENUM_END";
+})(SelfOP || (SelfOP = {}));
 
 router.ws('/', (ws) => {
-    var view = bufferify.encode(1, {time: new Date().getTime()}),
+    var view = bufferify.encode(2, {time: new Date().getTime()}),
         interval;
-    view.setUint8(0, SystemReturnOP.RETURN_TIME);
+    view.setUint16(0, SystemReturnOP.RETURN_TIME);
     ws.send(view);
     interval = setInterval(() => {
-        var view = bufferify.encode(1, {
+        var view = bufferify.encode(2, {
             id: '3434',
             arr: [randRange(1, 5), randRange(3, 5), randRange(4, 5), randRange(50, 54), randRange(50, 56)],
             obj: {
@@ -43,20 +58,36 @@ router.ws('/', (ws) => {
                 lv: randRange(12, 20)
             }
         });
-        view.setUint8(0, TestReturnOP.RETURN_INFO);
+        view.setUint16(0, TestReturnOP.RETURN_INFO);
         ws.send(view);
     }, 500);
     ws.on('message', (data) => {
         var opcode;
         if (data instanceof Buffer) {
             view = new DataView(new Uint8Array(data).buffer);
-            opcode = view.getUint8(0);
-            if (opcode >= TestSendOP.ENUM_END || opcode <= SystemSendOP.ENUM_START) {
+            opcode = view.getUint16(0);
+            if (opcode > SelfOP.ENUM_START && opcode < SelfOP.ENUM_END) {
+                switch (opcode) {
+                    case SelfOP.SELF_HEELO:
+                        data = bufferify.decode(2, {
+                            msg: 'string',
+                            from: 'string'
+                        }, view);
+                        console.log(`${data.from}:${data.msg}`);
+                        view = bufferify.encode(2, {
+                            msg: `Nice to meet you too!`,
+                            from: 'server'
+                        });
+                        break;
+                }
+                view.setUint16(0, opcode);
+                ws.send(view);
+            } else if (opcode <= SystemSendOP.ENUM_START || opcode >= TestSendOP.ENUM_END) {
                 ws.close(1008, `Unknown opcode ${opcode}`);
             } else {
                 switch (opcode) {
                     case TestSendOP.SEND_JSON:
-                        view.setUint8(0, TestReturnOP.RETURN_JSON);
+                        view.setUint16(0, TestReturnOP.RETURN_JSON);
                         ws.send(view);
                         break;
                 }
